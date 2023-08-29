ce0cf94bf156c7d2a8db1ed534f786066660d8df
RodolfoAssam <rodozahir.dbd@gmail.com>
2023-08-29 17:07:09 -0500

    Nuevos cambios

---
+++ Parcial 2/ModuloESM/index.js
@@ -0,0 +1,16 @@
+let numerosenfrances=[
+    "Un",
+    "Deux",
+    "Trois",
+    "Quatre",
+    "Cinq",
+    "Six",
+    "Sept",
+    "Huit",
+    "Neuf",
+    "Dix",
+];
+
+export function obtieneNumeros(indice){
+    return numerosenfrances[indice];
+}
