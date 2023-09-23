6d388540f1c7c546d94fe343126ec8518e9ce40a
RodolfoAssam <rodozahir.dbd@gmail.com>
2023-09-22 19:36:30 -0500

    Nuevos cambios

--- Parcial 1/Grid/index.js
+++ Parcial 1/Grid/index.js
@@ -1,28 +1,38 @@
 const express = require('express');
 const cors = require('cors');
+const bearerToken = require('express-bearer-token');
 
-const {promisePool} = require('./connection.js');
+const { promisePool } = require('./connection.js');
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.get("/ServidorExpress/carro",(req,res)=>{
+app.use(bearerToken());
+
+app.get("/ServidorExpress/carro", (req, res) => {
     console.log(req.params);
     console.log(req.query);
     console.log(req.body);
-    res.send("Servidor express contestando a peticion GET")
+    res.send("Servidor express contestando a peticion GET");
 });
 
-app.get("/carros",async(req,res)=>{
-    const [query] = await promisePool.query('SELECT * from carro')
-
-    console.log(query)
-    res.json(query) 
-})
-
-app.post("/ServidorExpress",(req,res)=>{res.send("Servidor express contestando a peticion GET")
+app.get("/carros", async (req, res) => {
+    const [query] = await promisePool.query('SELECT * from carro');
+    console.log(query);
+    res.json(query);
 });
 
-app.listen(8080,(req,res)=>{
-    console.log("Servidor express escuchando")
-})
+//Bearer aqui verifico el token
+//token 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
+//http://localhost:8080/github.api
+//https://api.github.com/users/RodolfoAssam/repos
+app.get("/github.api", (req, res) => {
+    if (!req.token) {
+        return res.status(401).json({ error: 'Token de autenticación requerido' });
+    }
+    res.send("Servidor express contestando a peticion GET");
+});
+
+app.listen(8080, (req, res) => {
+    console.log("Servidor express escuchando");
+});
