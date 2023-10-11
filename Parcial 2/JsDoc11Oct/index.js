ef545928a5568a54abd5f950c09472b760906433
RodolfoAssam <rodozahir.dbd@gmail.com>
2023-10-11 10:16:53 -0500

    Nuevos cambios

---
+++ Parcial 2/JsDoc11Oct/index.js
@@ -0,0 +1,25 @@
+
+let numerosenfrances=[
+    "Un",
+    "Deux",
+    "Trois",
+    "Quatre",
+    "Cinq",
+    "Six",
+    "Sept",
+    "Huit",
+    "Neuf",
+    "Dix",
+]
+
+/**
+ * 
+ * @param {number} indice Numeros en frances 
+ * @returns 
+ */
+
+function obtieneNumeros(indice){
+    return numerosenfrances(indice);
+}
+
+module.exports.obtieneNumeros = obtenerNumero
