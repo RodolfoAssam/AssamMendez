f5b66806c4aa2ef2eb7bd1905d86a77d45e592a5
RodolfoAssam <rodozahir.dbd@gmail.com>
2023-11-23 21:06:02 -0600

    Nuevos cambios

--- Parcial 3/Jester/test.js
+++ Parcial 3/Jester/test.js
@@ -26,3 +26,14 @@ const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 
 let app='http://localhost:8080'
+
+describe("Pruebas al metodo get de la ruta de usuario", () => {
+    it("Si hago un get a la ruta usuario me debe dar un status 200",()=>{
+        chai.request(app)
+            .get("/carro")
+            .end((err,res) => {
+              expect(err).to.be.null;
+              expect(res).to.have.status(200);
+          });
+    });
+});
